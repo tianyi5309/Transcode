be474152fde4e52d8c64c6155700165329597935
tianyi <tianyi@tianyi.local>
2017-03-11 12:48:42 +0800

    update setup.sh to set transcode.service to executable

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ sudo apt-get install handbrake-cli -y
 sudo wget https://raw.githubusercontent.com/tianyi5309/Transcode/master/transcode.sh -O /usr/bin/transcode.sh
 sudo wget https://raw.githubusercontent.com/tianyi5309/Transcode/master/transcode.service -O /etc/systemd/system/transcode.service
 sudo chmod +x /usr/bin/transcode.sh
+sudo chmod 755 /etc/systemd/system/transcode.service
 sudo systemctl enable transcode.service
