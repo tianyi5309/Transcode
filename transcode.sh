8c8533051fbbd3408e646126fe392a48f2a8fc1d
tianyi <tianyi@tianyi.local>
2017-03-11 13:09:18 +0800

    hardcoded file paths

--- transcode.sh
+++ transcode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-HandBrakeCLI -i movie.mp4 -o output.mp4 -e x264 -q 20 -B 160 --encoder-preset fast
-scp -o StrictHostKeyChecking=no ~/output.mp4 root@tianyi.io:/root/transcodes/movie.mp4
+HandBrakeCLI -i /home/tianyi/movie.mp4 -o /home/tianyi/output.mp4 -e x264 -q 20 -B 160 --encoder-preset fast
+scp -o StrictHostKeyChecking=no /home/tianyi/output.mp4 root@tianyi.io:/root/transcodes/movie.mp4
 gcloud compute instances delete preempt-8-core --zone=asia-east1-a --quiet
