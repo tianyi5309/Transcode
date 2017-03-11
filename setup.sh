9ca51ce210d72ca892a509adbaecf4d3bc853d52
tianyi <tianyi@tianyi.local>
2017-03-11 12:38:32 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 sudo apt-get install handbrake-cli -y
-sudo echo "[Service]
-ExecStart=/usr/bin/transcode.sh" > /etc/systemd/system/transcode.service
-sudo echo "#!/bin/bash
-HandBrakeCLI -i movie.mp4 -o output.mp4 -e x264 -q 20 -B 160 --encoder-preset fast; scp -o StrictHostKeyChecking=no ~/output.mp4 root@tianyi.io:/root/transcodes/movie.mp4; gcloud compute instances delete preempt-8-core --zone=asia-east1-a --quiet" > /usr/bin/transcode.sh
+sudo wget https://raw.githubusercontent.com/tianyi5309/Transcode/master/transcode.sh -O /usr/bin/transcode.sh
+sudo wget https://raw.githubusercontent.com/tianyi5309/Transcode/master/transcode.service -O /etc/systemd/system/transcode.service
 sudo chmod +x /usr/bin/transcode.sh
 sudo systemctl enable transcode.service
