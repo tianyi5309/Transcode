ffaed67d884a57b2413b4fce3b73b72cb161b82a
tianyi <tianyi@tianyi.local>
2017-03-12 15:11:44 +0800

    updated to use scaleway server

--- transcode.sh
+++ transcode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 HandBrakeCLI -i /home/tianyi/movie -o /home/tianyi/output -e x264 -q 20 -B 160 --encoder-preset fast
-scp -o StrictHostKeyChecking=no -i /home/tianyi/.ssh/id_rsa /home/tianyi/output root@tianyi.io:/root/transcodes/movie
-gcloud compute instances delete preempt-8-core --zone=asia-east1-a --quiet
+scp -o StrictHostKeyChecking=no -i /home/tianyi/.ssh/id_rsa /home/tianyi/output root@tau.tianyi.io:/root/transcodes/movie
+gcloud compute instances delete preempt-8-core --zone=europe-west1-d --quiet
