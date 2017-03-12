6971e0ac8fe74e06c3979aea0be0fbe9a09bfd6e
tianyi <tianyi@tianyi.local>
2017-03-12 16:02:14 +0800

    Added audio config --all-audio

--- transcode.sh
+++ transcode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-HandBrakeCLI -i /home/tianyi/movie -o /home/tianyi/output -e x264 -q 20 -B 160 --encoder-preset fast -f av_mp4
+HandBrakeCLI -i /home/tianyi/movie -o /home/tianyi/output -e x264 -q 20 -B 160 --encoder-preset fast -f av_mp4 --all-audio
 scp -o StrictHostKeyChecking=no -i /home/tianyi/.ssh/id_rsa /home/tianyi/output root@tau.tianyi.io:/root/transcodes/movie
 gcloud compute instances delete preempt-8-core --zone=europe-west1-d --quiet
